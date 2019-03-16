20cd2c39b9451a5d9f58b2ecddaf5cf840915579
BlackJibert <1257699625@qq.com>
2019-03-16 18:16:20 +0800

    关于xpath和pyquery的修改

--- myspider/Parsing_library/PyQuery_pratices/crawl/4.3.4.2.py
+++ myspider/Parsing_library/PyQuery_pratices/crawl/4.3.4.2.py
@@ -1,10 +1,10 @@
 from pyquery import PyQuery as pq
 
 doc = pq(url='https://www.toutiao.com/')
-print(doc('title'))
+print(doc('title').text())
 
-# 和下面的是一样的
-import requests
-
-doc = pq(requests.get('https://www.toutiao.com/').text)
-print(doc('title'))
+# 和上面的是一样的
+# import requests
+#
+# doc = pq(requests.get('https://www.toutiao.com/').text)
+# print(doc('title'))
